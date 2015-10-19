8c089b7cca98e460ae6dbb4e9d1afae52dec20ae
alejandromartincruz <socorreitor@gmail.com>
2015-10-19 17:06:20 +0200

    Finished the first iteration

--- Sinatra_todo.rb
+++ Sinatra_todo.rb
@@ -5,28 +5,68 @@ class Todolist
 end
 
 class Task
-	attr_reader :content, :id
+	attr_reader :content, :id, :done
     @@current_id = 1
     def initialize(content)
         @content = content
         @id = @@current_id
         @@current_id += 1
-        @id
+        @done = false
     end
+
+    def completed?
+   		@done
+    end
+
+    def complete!
+    	@done=true
+    end
+
+    def make_incomplete!
+    	@done=false
+    end
+
 end
 
 
 describe Todolist do 
 
-#before :each do
+before :each do
 #	@Todo = Task.new
-#end
+	@task1 = Task.new("alguna cosa tendre que hacer")
+	@task2 = Task.new("luego hare algo mas")
+	@task3 = Task.new("y luego toca descansar")
+end
 
   describe "#CreatingTask" do
     it "Creates a new task and gives back an unique Id" do
-      expect(Task.new("alguna cosa tendre que hacer").id).to eq(1)
-      expect(Task.new("luego hare algo mas").id).to eq(2)
-      expect(Task.new("y luego toca descansar").id).to eq(3)
+      expect(@task1.id).to eq(1)
+      expect(@task2.id).to eq(2)
+      expect(@task3.id).to eq(3)
+    end
+
+    it "It cheack if the task is complete" do
+      expect(@task1.done).to eq(false)
+      expect(@task2.done).to eq(false)
+      expect(@task3.done).to eq(false)
+    end
+
+    it "It cheack if the task is complete" do
+      expect(@task1.completed?).to eq(false)
+      expect(@task2.completed?).to eq(false)
+      expect(@task3.completed?).to eq(false)
+    end
+
+    it "It marks a task as complete" do
+      expect(@task1.complete!).to eq(true)
+      expect(@task2.complete!).to eq(true)
+      expect(@task3.complete!).to eq(true)
+    end
+
+    it "It marks a task as complete" do
+      expect(@task1.make_incomplete!).to eq(false)
+      expect(@task2.make_incomplete!).to eq(false)
+      expect(@task3.make_incomplete!).to eq(false)
     end
 
   end
